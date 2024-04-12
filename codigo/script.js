0ff7164dea55b73c19280943f4400f15644cffb8
Karolline Carvalho <103125420+KarollineCS@users.noreply.github.com>
2024-04-12 12:32:39 -0300

    Update script.js

--- codigo/script.js
+++ codigo/script.js
@@ -14,3 +14,17 @@ function slider() {
 }
 
 setInterval(slider, 3000);  
+
+document.addEventListener('DOMContentLoaded', () => {
+    const toggle = document.getElementById('toggle');
+
+    toggle.addEventListener('change', () => {
+        document.body.classList.toggle('darkmode');
+        document.querySelector('.header_navigation').classList.toggle('darkmode');
+        document.querySelector('.section-news').classList.toggle('darkmode');
+        document.querySelector('.price').classList.toggle('darkmode');
+        document.querySelector('.section-gif').classList.toggle('darkmode');
+        document.querySelector('.recomendations').classList.toggle('darkmode');
+        document.querySelector('.recomendations button').classList.toggle('darkmode');
+    });
+});
